cf19aa8179a76456d140e13a95686b5a278cf97f
Oleg <oleg@localhost>
2022-10-11 03:20:33 +0300

    add new file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,13 @@
 (async () => {
+<<<<<<< HEAD
     // TODO: add custom logic here
+=======
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            const coordsEl = document.querySelector('#user-coords');
+            coordsEl.textContent = `${position.coords.latitude}, ${position.coords.longitude}`
+            coordsEl.parentElement.classList.remove('invisible');
+        });
+    }
+>>>>>>> 7516f31 (Geolocation added)
 })();
